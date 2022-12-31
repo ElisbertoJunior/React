f758d48bef49eb03dec2fb5b11d03176a645cee4
Elisberto Junior <elisberto_jr@hotmail.com>
2022-12-30 21:11:00 -0300

    first commit

---
+++ projeto/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola React</h1>
+      <Button title="Clique aqui!"></Button>
+      <Button title="Clique aqui!"></Button>
+      <Button title="Clique aqui!"></Button>
+    </div>
+  );
+}
+
+export default App;
