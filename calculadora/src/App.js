de31b5a280c55252e4b23323bcbcff9b45a95a65
Elisberto Junior <elisberto_jr@hotmail.com>
2022-12-31 17:01:43 -0300

    create calculadora

---
+++ calculadora/src/App.js
@@ -0,0 +1,17 @@
+import Input from "./components/Input";
+import Button from "./components/Button";
+import { Container, Content } from "./styled";
+
+
+const App = () => {
+  return (
+    <Container>
+      <Content>
+          <Input/>
+          <Button/>
+      </Content>
+    </Container>
+  );
+};
+
+export default App;
