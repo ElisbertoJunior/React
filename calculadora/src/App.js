5a56a9a08e98e395dcb2e97eac31bfee12756193
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-03 22:14:05 -0300

    finalizando exercicio

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -20,18 +20,13 @@ const App = () => {
   }
 
   const handleSumNumbers = () => {
-
     if (firstNumber === '0') {
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0')
-      setOperation('+')
-      
+      setOperation('+') 
     } else {
-
     const sum = Number(firstNumber) + Number(currentNumber);
-
     setCurrentNumber(String(sum));
-
     setFirstNumber('0')
     setOperation('')
 
@@ -40,21 +35,43 @@ const App = () => {
   }
 
   const handleMinusNumbers = () => {
-
     if (firstNumber === '0') {
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0')
-      setOperation('-')
-      
+      setOperation('-')   
     } else {
-
     const minus = Number(firstNumber) - Number(currentNumber);
-
     setCurrentNumber(String(minus));
-
     setFirstNumber('0')
     setOperation('')
+    }
 
+  }
+
+  const handleMultiplyNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('*')   
+    } else {
+    const multiply = Number(firstNumber) * Number(currentNumber);
+    setCurrentNumber(String(multiply));
+    setFirstNumber('0')
+    setOperation('')
+    }
+
+  }
+
+  const handleDividedNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('/')   
+    } else {
+    const divided = Number(firstNumber) / Number(currentNumber);
+    setCurrentNumber(String(divided));
+    setFirstNumber('0')
+    setOperation('')
     }
 
   }
@@ -66,6 +83,19 @@ const App = () => {
         case '+':
           handleSumNumbers();
           break;
+
+          case '-':
+          handleMinusNumbers();
+          break;
+
+          case '*':
+          handleMultiplyNumbers();
+          break;
+
+          case '/':
+          handleDividedNumbers();
+          break;
+
           default:
             break;
       }
@@ -74,22 +104,21 @@ const App = () => {
   }
 
 
-
   return (
     <Container>
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x" onClick={() => handleAddNumber('')}/>
-          <Button label="/" onClick={() => handleAddNumber('')}/>
-          <Button label="C" onClick={handleOnClear}/>
-          <Button label="X" onClick={() => handleAddNumber('')}/>
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="/" onClick={handleDividedNumbers}/>
+          <Button label="c" onClick={handleOnClear}/>
+          <Button label="x" onClick={handleMultiplyNumbers}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={() => handleAddNumber('-')}/>
+          <Button label="-" onClick={handleMinusNumbers}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
