2c6ddd6481441ee267e48489e8556fc68e4ab0d1
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-08 22:55:20 -0300

    add cilclo-de-vida

--- ciclo-de-vida/src/App.js
+++ ciclo-de-vida/src/App.js
@@ -1,20 +1,34 @@
-import React, { useState, usefect } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [user, setUser] = useState("Elisberto");
-  const [users, setUsers] = useState(["Elsiberto", "Jose", "Manoel"])
+  const [user, setUser] = useState("");
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState(["Elisberto", "Jose", "Manoel"])
 
+  const handleAddUser = () => {
+    setUsers([...users, user]);
+    setUser("");
+  };
+
+  useEffect(() => {
+    setCount(users.length)
+  }, [users])
 
   return (
     <div className="App">
-      <h>Hello World!</h>
+      <h1>Hello World!</h1>
+      <h3>Total: {count} </h3>
 
     <div>
       <input
         value={user}
         onChange={(event)=> setUser(event.target.value)}
       />
+      <button onClick={handleAddUser}>Adicionar</button>
     </div>
+    <hr/>
+
+    {users.map(item => <p>{item}</p>)}
       
     </div>
   );
