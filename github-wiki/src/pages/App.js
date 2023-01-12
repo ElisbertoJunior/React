51b55c833d9b9b2dd44ffb5b0dd19140b4d67b84
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-11 22:21:32 -0300

    estizando components

--- github-wiki/src/pages/App.js
+++ github-wiki/src/pages/App.js
@@ -1,9 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
 import gitLogo from '../assets/github.png'
+import { Container } from './styles';
 
 const App =() => {
   return (
     <div className="App">
-      <img src={gitLogo} width={72} height={72}/>
+      <Container>
+        <img src={gitLogo} width={72} height={72} alt="GitHub Logo"/>
+      </Container>
     </div>
   );
 }
