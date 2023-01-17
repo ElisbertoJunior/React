b71928c0c39a3da9ae08d60bf7467477edc1893e
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-16 22:52:39 -0300

    finalizando

---
+++ github-wiki/src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { InputContainer } from './styles'
+
+const Button = () =>  {
+  return (
+    <ButtonContainer>
+      <Button/>
+    </ButtonContainer>
+  )
+}
+
+export default Button 
--- github-wiki/src/pages/App.js
+++ github-wiki/src/pages/App.js
@@ -1,13 +1,20 @@
 import gitLogo from '../assets/github.png'
 import Input from '../components/Input';
+import ItemRepo from '../components/ItemRepo';
+import Button from '../components/Button';
 import { Container } from './styles';
 
-const App =() => {
+const App = () => {
+
+const [repos, setRepos] = useState([]);
+
   return (
     <div className="App">
       <Container>
         <img src={gitLogo} width={72} height={72} alt="GitHub Logo"/>
-        <Input></Input>
+        <Input />
+        <Button/>
+        <ItemRepo/>
       </Container>
     </div>
   );
