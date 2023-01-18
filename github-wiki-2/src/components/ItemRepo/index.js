ac5c2f9ecc85ecd2b187b4ac7858c4b0d272ee21
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-17 21:23:32 -0300

    finalizando wiki

--- github-wiki-2/src/components/ItemRepo/index.js
+++ github-wiki-2/src/components/ItemRepo/index.js
@@ -1,12 +1,17 @@
 import { ItemContainer } from "./styles"
 
 
-const ItemRepo = ({ repo }) => {
+const ItemRepo = ({ repo, handleRemoveRepo }) => {
+
+const handleRemove = () => {
+    handleRemoveRepo(repo.id)
+}
+
   return (
-    <ItemContainer>
-      <h3>Elisberto</h3>
-      <p>Junior</p>
-      <a href="#">Ver repositorio</a> <br/>
+    <ItemContainer onClick={handleRemove}>
+      <h3>{repo.name}</h3>
+      <p>{repo.full_name}</p>
+      <a href={repo.html_url} target="blank">Ver repositorio</a> <br/>
       <a href="#" className="remove">Remover</a>
       <hr/>
     </ItemContainer>
--- github-wiki-2/src/pages/App.js
+++ github-wiki-2/src/pages/App.js
@@ -16,9 +16,24 @@ const App = () => {
     const { data } = await api.get(`repos/${currentRepo}`)
 
     if (data.id) {
-       setRepos(prev => [...prev, data])      
+
+      const isExist = repos.find(repo => repo.id === data.id);
+
+      if (!isExist) {
+        setRepos(prev => [...prev, data]);
+        setCurrentRepo('');    
+        return 
+      }
+
     }
 
+    alert("Repositorio nao encontrado.");
+
+  }
+
+  const handleRemoveRepo = (id) => {
+    const newList = repos.filter(repo => repo.id !== id)
+    setRepos([...newList]);
   }
 
   return (
@@ -27,7 +42,7 @@ const App = () => {
           <img src={gitLogo} width={72} height={72} alt="GitHub Logo"/>
           <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)}/>
           <Button onClick={handleSearchRepo}/>
-          {repos.map(repo => <ItemRepo repo={repo}/>)}
+          {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
         </Container>
     </div>
   );                                    
