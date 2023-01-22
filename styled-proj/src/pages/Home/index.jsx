7fe13988cb61b33356d050cf9677d4b58168b028
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-22 14:25:22 -0300

    finalizando

--- styled-proj/src/pages/Home/index.jsx
+++ styled-proj/src/pages/Home/index.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Button from '../../components/Button';
 import Header from '../../components/Header';
+import { Container, Title, TitleHighLight, TextContent } from './styles'
 
  const Home = () => {
   return (
-    <div>
+    <>
       <Header/>
+      <Container>
+        <div>
+          <Title>
+            <TitleHighLight>
+              Implemente
+              <br/>  
+            </TitleHighLight>
+            O seu futuro global agora!
+          </Title>
+          <TextContent>
+            Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo
+            e encare seu novo desafio profissional, evoluindo com os melhores experts.
+          </TextContent>
+          <Button title='Comecar agora' variant="secondary" onClick={() => null}/>
+        </div>
+        <div>
+          <img src="#" alt="Imagem principal" />
+        </div>
+      </Container>
     
-    </div>
+    </>
   )
 }
 
