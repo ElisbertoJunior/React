5393a34738c819739c06a87cdcc9c436ca42b9d0
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-21 21:56:04 -0300

    finalizando

---
+++ styled-proj/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Button from '../../components/Button';
+
+ const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button title="teste 1" />
+      <Button title="teste 2"variant='secondary' />
+    </div>
+  )
+}
+
+export default Home;
